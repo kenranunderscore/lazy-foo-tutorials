a36a5b6754bbc66cb3598e6ba092f182c1a9e47d
kenranunderscore <johb.maier@gmail.com>
2019-08-25 12:15:11 +0200

    Move OCaml stuff into separate directory

--- ocaml/lesson_03/main.ml
+++ ocaml/lesson_03/main.ml
@@ -32,6 +32,6 @@ let _ =
   Utils.with_sdl @@ fun _ ->
   Utils.with_window screen_width screen_height "SDL Tutorial" @@ fun window ->
   Utils.with_window_surface window @@ fun screen_surface ->
-  Utils.with_bitmap "resources/x.bmp" @@ fun bitmap_surface ->
+  Utils.with_bitmap "../resources/x.bmp" @@ fun bitmap_surface ->
   event_loop window screen_surface bitmap_surface ();
   Ok ()
