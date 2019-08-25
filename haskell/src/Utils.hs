237845ac94de885a6aec4872790a10791d3f62b9
kenranunderscore <johb.maier@gmail.com>
2019-08-25 21:26:26 +0200

    Refactor the Utils module some more

--- haskell/src/Utils.hs
+++ haskell/src/Utils.hs
@@ -1,4 +1,10 @@
-module Utils where
+module Utils
+  ( withSdl
+  , withWindow
+  , withWindowSurface
+  , withBitmapSurface
+  )
+where
 
 import           Relude
 
@@ -9,32 +15,24 @@ import qualified SDL
 
 withSdl :: [SDL.InitFlag] -> IO a -> IO a
 withSdl flags = Exception.bracket_
-  (putTextLn "Initializing SDL..." >> SDL.initialize flags)
-  (putTextLn "Quitting SDL..." >> SDL.quit)
+  (withMessage "Initializing SDL..." (SDL.initialize flags))
+  (withMessage "Quitting SDL..." SDL.quit)
 
 withWindow :: Text -> SDL.WindowConfig -> (SDL.Window -> IO a) -> IO a
 withWindow title windowConfig = Exception.bracket
-  (do
-      putTextLn $ "Creating window '" <> title <> "'..."
-      SDL.createWindow title windowConfig)
-  (\window -> do
-      putTextLn $ "Destroying window '" <> title <> "'..."
-      SDL.destroyWindow window)
+  (withMessage ("Creating window '" <> title <> "'...") (SDL.createWindow title windowConfig))
+  (withMessage ("Destroying window '" <> title <> "'...") . SDL.destroyWindow)
 
 withWindowSurface :: SDL.Window -> (SDL.Surface -> IO a) -> IO a
 withWindowSurface window = Exception.bracket
-  (do
-      putTextLn "Getting window surface..."
-      SDL.getWindowSurface window)
-  (\surface -> do
-      putTextLn $ "Destroying window surface..."
-      SDL.freeSurface surface)
+  (withMessage "Getting window surface..." (SDL.getWindowSurface window))
+  (withMessage "Destroying window surface..." . SDL.freeSurface)
 
 withBitmapSurface :: FilePath -> (SDL.Surface -> IO a) -> IO a
 withBitmapSurface path = Exception.bracket
-  (do
-      putTextLn $ "Loading bitmap surface '" <> toText path <> "'..."
-      SDL.loadBMP path)
-  (\surface -> do
-      putTextLn $ "Destroying bitmap surface '" <> toText path <> "'..."
-      SDL.freeSurface surface)
+  (withMessage ("Loading bitmap surface '" <> toText path <> "'...") (SDL.loadBMP path))
+  (withMessage ("Destroying bitmap surface '" <> toText path <> "'...") . SDL.freeSurface)
+
+withMessage :: Text -> IO a -> IO a
+withMessage message action = do
+  putTextLn message >> action
